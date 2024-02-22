9bdbe545a0009e8d176ab3632ed5511fd9037a36
haiban99200 <125704152+haiban99200@users.noreply.github.com>
2024-02-22 15:16:29 +0700

    Add files via upload
    new comment

---
+++ project x.py
@@ -0,0 +1,5 @@
+# from web3 import Web3, AsyncWeb3
+# import json
+
+
+# ABI = '[{"inputs":[{"internalType":"string","name":"name_","type":"string"},{"internalType":"string","name":"symbol_","type":"string"},{"internalType":"uint256","name":"totalSupply_","type":"uint256"},{"internalType":"address","name":"rewardToken_","type":"address"},{"internalType":"address","name":"router_","type":"address"},{"internalType":"uint256[5]","name":"feeSettings_","type":"uint256[5]"},{"internalType":"address","name":"serviceFeeReceiver_","type":"address"},{"internalType":"uint256","name":"serviceFee_","type":"uint256"}],"stateMutability":"payable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},
