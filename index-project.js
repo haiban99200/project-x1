b51db8a409745f66465edd7702744ecff958bdd5
haiban99200 <125704152+haiban99200@users.noreply.github.com>
2024-03-18 15:26:00 +0700

    Add files via upload
    Signed-off-by: haiban99200 <125704152+haiban99200@users.noreply.github.com>

---
+++ index-project.js
@@ -0,0 +1,24 @@
+var cusid_ele = document.getElementsByClassName(
+  "css-901oao css-1hf3ou5 r-14j79pv r-18u37iz r-37j5jr r-1wvb978 r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0"
+);
+for (var i = 0; i < cusid_ele.length; ++i) {
+  var item = cusid_ele[i];
+  item.innerHTML;
+  console.log("x");
+}
+
+setInterval(function () {
+  var cusid_ele = document.getElementsByClassName(
+    "css-901oao css-1hf3ou5 r-14j79pv r-18u37iz r-37j5jr r-1wvb978 r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0"
+  );
+  for (var i = 0; i < cusid_ele.length; ++i) {
+    var item = cusid_ele[i];
+    item.innerHTML;
+  }
+}, 2000);
+
+for (var i = 0; i < 10; ++i) {
+  // document.getElementsByClassName('css-901oao css-1hf3ou5 r-14j79pv r-18u37iz r-37j5jr r-1wvb978 r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0')[i].innerText
+  console.log(i);
+  console.log("x");
+}
